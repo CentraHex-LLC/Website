ba89daa193632c1e2c0b7017559f144aa6ea52d2
Grey Johnson <contact@greyjohnson.dev>
2022-04-25 15:05:56 -0400

    added image to website banner

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header id="header" className="alt">
-      <span className="logo logo-custom"><img src="banner.svg" alt=""  /></span>
-    </header>
+    <div className="header-custom">
+      <img src='splash.jpg' className='custom-image'></img>
+      <header id="header" className="alt">
+        <span className="logo logo-custom"><img src="banner.svg" alt=""  /></span>
+      </header>
+    </div>
   );
 };
 
