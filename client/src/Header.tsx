bcd46fae7ed3c12dd779be40537f40a5f523ef66
Grey Johnson <contact@greyjohnson.dev>
2022-04-25 15:08:33 -0400

    scale down image

--- client/src/Header.tsx
+++ client/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header-custom">
-      <img src='splash.jpg' className='custom-image'></img>
+      <img src='splash-min.jpg' className='custom-image'></img>
       <header id="header" className="alt">
         <span className="logo logo-custom"><img src="banner.svg" alt=""  /></span>
       </header>
